005a4a633f182640e8f522b8bc520b09a74fe94a
Dave Taht <dave.taht@bufferbloat.net>
2014-01-07 10:28:57 -0800

    Basic ecn test outline.
    It's easy after you establish a circuit...

---
+++ test_ecn.c
@@ -0,0 +1,14 @@
+/* Test survival of ecn marked bits
+
+   Might be able to assemble this with sendmsg.
+
+   for(int i = 0; i < 3; i++) {
+	settos(socket,i);
+	send_packet()
+   }
+   recv_confirmation()
+  
+*/ 
+
+
+
