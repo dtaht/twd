ab277ce746402a5d5aa425308ae1292141088c02
Dave Taht <dave.taht@bufferbloat.net>
2014-01-08 09:52:31 -0500

    twd-remote outline
    toke suggested there be a remotely controllable version of twd.
    
    So I agree, something like transmission-remote would be nice.
    
    I will argue that the basic functionality of the core needs to settle
    down first, that we need the udp client/server infrastructure, tests,
    and protocol. But I won't stop anyone from starting...

---
+++ twd-remote.c
@@ -0,0 +1,6 @@
+/* Placeholder for remotely controllable version of twd.
+   the twd options is an object for this reason. My take on it
+   is that this would listen on a ssl tcp socket, and take json
+   encodings of the command line options.
+
+*/
