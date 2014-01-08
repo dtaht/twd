9a11908a453e53a1115f8760728593384e3dee4c
Dave Taht <dave.taht@bufferbloat.net>
2014-01-08 08:19:03 -0500

    outline for testing a call for loss and delay and for fq

---
+++ test_call.c
@@ -0,0 +1,13 @@
+/* Test an isochronous stream
+
+   setup_interval(hook,10ms); // much needed magic elided see client.c for wip
+
+hook() {
+   get_time()
+   if(duration < end_of_test) send_packet();
+ }
+ 
+*/ 
+
+
+
---
+++ test_fq.c
@@ -0,0 +1,15 @@
+/* Test fq by setting up one high rate and one low rate stream
+
+   setup_interval(hook,100ms); // 
+   flood(duration);
+
+hook() {
+   get_time()
+   if(duration < end_of_test) send_packet();
+ }
+ 
+   see if rtts stay somewhat flat for the slow stream
+*/ 
+
+
+
