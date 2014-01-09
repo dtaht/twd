343d0ad9ddcf6d445d98ea20886519082a62e541
Dave Taht <dave.taht@bufferbloat.net>
2014-01-09 05:38:38 -0500

    Added outline of test_self.c
    I strongly suspect many OSes and older versions will silently
    twiddle the dscp and tos settings. So testing the self stuff
    adds an innate host OS test and regression test. I'll argue
    this is the first test we need to complete...
    
    (but first up is the essential client/server udp plumbing code,
     then the protocol (but we can fudge that))
    
    There is no point in writing the rest of this unless the
    core tests can be made to work on a given OS.

---
+++ test_self.c
@@ -0,0 +1,10 @@
+/* Do some self tests and timings
+   Measure:
+
+   syscall overhead
+   get_time resolution (already in test_clock)
+   preservation of ecn and DSCP bits through localhost (important!
+	many OSes silently pee on these bits)
+   bandwidth via localhost
+   */
+
