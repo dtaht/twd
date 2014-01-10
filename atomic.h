04e2e7994e1da794a7033c9531198a1d3639a998
Dave Taht <dave.taht@bufferbloat.net>
2014-01-10 12:23:56 -0500

    Basic atomic support

--- atomic.h
+++ atomic.h
@@ -4,7 +4,8 @@
    I think this was finally standardized in c11 */
 
 #define atomic_compare_and_swap __sync_val_compare_and_swap
-#define atomic_fetch_and_add    __sync_fetch_and_add
-#define atomic_sync __sync_synchronize
+#define atomic_sub(a,b)		__sync_sub_and_fetch(a,b)
+#define atomic_add(a,b)		__sync_add_and_fetch(a,b)
+#define atomic_sync		__sync_synchronize
 #endif 
 
