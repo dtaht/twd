7053ff62ae9dc0a964bfe977418559696c13e392
Dave Taht <dave.taht@bufferbloat.net>
2014-01-12 22:28:43 -0500

    log: need even more generic logging warning and error functions than this
    needs to run as a network daemon so merely passing file descriptors
    around won't work. Something like this filled out...
    
    struct cb {
    	(void *) *warn ( int err, char * message) );
    	(void *) *info ( int err, char * message) );
    	(void *) *error ( int err, char * message) );
    }
    
    and a template for various normal, network, json
    call backs to fit into that (think my syntax is wrong
    above anyway)
    
    then we'd use it with
    
    log_warn(o->cb,errno, "message");

---
+++ log.h
@@ -0,0 +1,11 @@
+
+
+#define handle_error_en(en, msg) \
+  do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+#define handle_warning_en(en, msg) \
+  do { errno = en; perror(msg); } while (0)
+
+#define handle_error(msg) \
+  do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
