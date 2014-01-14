b8133dab0e4c4888f0466ed5c6ee1a779682498c
Dave Taht <dave.taht@bufferbloat.net>
2014-01-13 22:42:21 -0500

    created simple switchable logger for console and syslog
    there will be a version of this that accumulates messages without printing
    or making any syscalls later.

---
+++ log.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+#include "log.h"
+
+#define __read_mostly
+
+/* All the verbosity of C with all the complexity of C++ */
+
+/* Console log error messages */
+
+struct log_control {
+  int facility;
+  int level;
+  int warn;
+  int error; 
+  int info;
+  int debug;
+  int debug_level;
+};
+
+typedef struct log_control log_control_t;
+
+static void *logger_not_avail(int err) {
+  fprintf(stderr,"Logger not available now: %d\n", err);
+  exit(err);
+  return NULL;
+}
+
+#define FULL_PRIV_CHECK(a, type) ( (type *) a == NULL ? logger_not_avail(-1) : \
+				   ((type *) a)->priv == NULL ?		\
+				   logger_not_avail(-1) : ((type *) a)->priv )
+
+#define PRIV_CHECK(a) FULL_PRIV_CHECK(a, logger_t)
+
+static void log_warn (logger_t *self, int err, char *message)
+{
+  log_control_t *priv = (log_control_t *) PRIV_CHECK(self);
+  priv->warn++;
+  fprintf(stderr,"WARN: %d: %s\n", err, message);
+}
+
+static void log_error (logger_t *self, int err, char *message)
+{
+  log_control_t *priv = (log_control_t *) PRIV_CHECK(self);
+  priv->error++;
+  fprintf(stderr,"ERROR: %d: %s\n", err, message);
+}
+
+static void log_info (logger_t *self, int err, char *message)
+{
+  log_control_t *priv = (log_control_t *) PRIV_CHECK(self);
+  priv->info++;
+  fprintf(stderr,"INFO: %d: %s\n", err, message);
+}
+
+static void log_debug (logger_t *self, int level, int err, char *message)
+{
+  log_control_t *priv = (log_control_t *) PRIV_CHECK(self);
+  priv->debug++;
+  if(priv->debug_level < level) 
+    fprintf(stderr,"DEBUG: %d %s\n", err, message);
+}
+
+static void log_init(logger_t *self, const char *name)
+{
+  if(self->priv == NULL) {
+    self->priv = calloc(sizeof(log_control_t),1);
+    log_control_t * priv = (log_control_t *) PRIV_CHECK(self);
+  }
+}
+
+static void log_reset(logger_t *self)
+{
+}
+
+static void log_destroy(logger_t *self)
+{
+  if(self != NULL) {
+    log_control_t * priv = (log_control_t *) PRIV_CHECK(self);
+    if(priv != NULL)
+      {
+	free(priv);
+      } 
+  }
+}
+
+static void log_change(logger_t *self)
+{
+}
+
+static void log_dump(logger_t *self)
+{
+}
+
+static void log_dump_stats(logger_t *self, void *stats)
+{
+  log_control_t *priv = (log_control_t *) PRIV_CHECK(self);
+  fprintf(stderr, "info: %d warn: %d error: %d debug %d messages sent\n", 
+	 priv->info, priv->warn, priv->error, priv->debug);
+}
+
+logger_t console_logger __read_mostly = {
+        .id             =       "std",
+        .priv_size      =       sizeof(log_control_t),
+
+        .warn           =       log_warn,
+        .error          =       log_error,
+	.info           =       log_info,
+        .debug          =       log_debug,
+
+        .init           =       log_init,
+        .reset          =       log_reset,
+        .destroy        =       log_destroy,
+        .change         =       log_change,
+        .dump           =       log_dump,
+        .dump_stats     =       log_dump_stats,
+};
+
+/* Syslog version (to make sean happy */
+
+
+struct syslog_control {
+  int facility;
+  int level;
+  int warn;
+  int error; 
+  int info;
+  int debug;
+  int debug_level;
+};
+
+typedef struct syslog_control syslog_control_t;
+
+static void syslog_warn (logger_t *self, int err, char *message) {
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+  priv->warn++;
+  syslog(LOG_WARNING, "%s\n", message);
+}
+
+static void syslog_error (logger_t *self, int err, char *message) {
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+  priv->error++;
+  syslog(LOG_ERR, "%s\n", message);
+}
+
+static void syslog_info (logger_t *self, int err, char *message) {
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+  priv->info++;
+  syslog(LOG_NOTICE, "%s\n", message);
+}
+static void syslog_debug (logger_t *self, int level, int err, char *message) {
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+  priv->debug++;
+  if(priv->debug_level < level) 
+    syslog(LOG_DEBUG, "%s\n", message);
+}
+
+static void syslog_init(logger_t *self, const char *name)
+{
+  printf("enabling syslogging");
+  openlog(name,LOG_CONS|LOG_NDELAY,LOG_LOCAL6);
+  if(self->priv == NULL) {
+    self->priv = calloc(sizeof(syslog_control_t),1);
+    syslog_control_t * priv = (syslog_control_t *) PRIV_CHECK(self);
+    priv->facility = LOG_CONS | LOG_NDELAY;
+    priv->level = LOG_LOCAL6;
+  }
+}
+
+static void syslog_reset(logger_t *self)
+{
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+}
+
+static void syslog_destroy(logger_t *self)
+{
+if(self != NULL) {
+    syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+    if(priv != NULL)
+      {
+	free(priv);
+      } 
+}
+closelog();
+}
+
+static void syslog_change(logger_t *self)
+{
+}
+
+static void syslog_dump(logger_t *self)
+{
+}
+
+static void syslog_dump_stats(logger_t *self, void *stats)
+{
+  syslog_control_t *priv = (syslog_control_t *) PRIV_CHECK(self);
+  syslog(LOG_INFO, "info: %d warn: %d error: %d debug %d messages sent\n", 
+	 priv->info, priv->warn, priv->error, priv->debug);
+}
+
+logger_t sys_logger __read_mostly = {
+        .id             =       "syslog",
+        .priv_size      =       sizeof(syslog_control_t),
+
+        .warn           =       syslog_warn,
+        .error          =       syslog_error,
+	.info           =       syslog_info,
+        .debug          =       syslog_debug,
+
+        .init           =       syslog_init,
+        .reset          =       syslog_reset,
+        .destroy        =       syslog_destroy,
+        .change         =       syslog_change,
+        .dump           =       syslog_dump,
+        .dump_stats     =       syslog_dump_stats,
+};
+
+#ifdef TEST
+
+int main() {
+logger_t logger = console_logger;
+logger.init(&logger,"TEST");
+WARN(3,"testing warnin");
+INFO(3,"testing info");
+ERR(3,"testing err");
+DEBUG(3,3,"testing debug");
+INFO(3,"Switching to syslog, tail /var/log/syslog to see");
+
+logger.destroy(&logger);
+logger = sys_logger;
+logger.init(&logger,"TEST");
+WARN(3,"testing warnin");
+INFO(3,"testing info");
+ERR(3,"testing err");
+DEBUG(3,3,"testing debug");
+logger.destroy(&logger);
+
+}
+#endif
--- log.h
+++ log.h
@@ -1,4 +1,5 @@
-
+#ifndef _TWD_LOG_H
+#define _TWD_LOG_H
 
 #define handle_error_en(en, msg) \
   do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -9,3 +10,43 @@
 #define handle_error(msg) \
   do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+// FIXME need varargs here
+
+#define WARN(err,message) logger.warn(&logger, err, message);
+#define INFO(err,message) logger.info(&logger, err, message);
+#define ERR(err,message) logger.error(&logger, err, message);
+#define DEBUG(level, err, message) logger.debug(&logger, level, err, message);
+
+// log_cb *
+
+typedef struct log_cb {
+  char * id;
+  int priv_size;
+  void (*warn) ( struct log_cb  *self,  int err, char * message);
+  void (*info) ( struct log_cb  *self,  int err, char * message);
+  void (*error) ( struct log_cb  *self, int err, char * message);
+  void (*debug) ( struct log_cb  *self, int level, int err, char * message);
+
+  /* this needs to be thunk out  */
+
+  void (*init) ( struct log_cb  *self, const char *name);
+  void (*reset) ( struct log_cb  *self);
+  void (*destroy) ( struct log_cb  *self);
+  void (*change) ( struct log_cb  *self);
+  void  (*dump) ( struct log_cb  *self);
+  void  (*dump_stats) ( struct log_cb  *self, void *stats);
+  void*  priv;
+
+} logger_t;
+
+// typedef struct log_cb logger_t;
+
+extern logger_t console_logger;
+extern logger_t sys_logger;
+
+/* 
+extern logger_t json_logger;
+extern logger_t twd_logger;
+*/
+
+#endif 
