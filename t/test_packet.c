b2ed4c548ebd25a1cfaba69cd8d1744a1a41362d
Dave Täht <dave.taht@bufferbloat.net>
2014-01-29 20:07:12 -0800

    start adding tests to t

---
+++ t/test_packet.c
@@ -0,0 +1,16 @@
+int main()
+{
+
+/* create a reader socket and a writer socket. No need to fork
+   this is a simple test */
+
+/* Turn on timestamping, tos, and ttl via setsockopt */
+
+/* write a packet */
+
+/* read a packet to see if the headers are preserved and the above 
+   sendmsg/recmsg code correct */
+
+/* end */
+
+}
